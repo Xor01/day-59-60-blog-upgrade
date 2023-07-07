bb14ce1c20b9f20420f6260e57df741d9aa81f33
Mohammed A <=>
2023-07-08 01:08:14 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/post.html')
+def post():
+    return render_template('post.html')
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run()
